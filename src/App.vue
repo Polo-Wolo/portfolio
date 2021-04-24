73ef537a8a5ec5c8c686014177ba4c1cfc6a165b
Polo <polo.walczak@gmail.com>
2021-04-24 23:04:40 +0200

    🚀 Nav Bar and links

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+	<div id="nav">
+		<router-link to="/"><img class="h-8 w-10 ml-16 transform hover:scale-105 hover:from-transparent" src="../src/assets/PW.svg"></router-link>
+		<div class="flex-grow"></div>
+		<div class="flex-inline space-x-16 items-center">
+			<router-link to="/">Home</router-link>
+			<router-link to="/education">Education</router-link>
+			<router-link to="/projects">Projects</router-link>
+			<router-link to="/hobbies">Hobbies</router-link>
+		</div>
+		<div class="btn btn-dodger ml-64">Contact</div>
+	</div>
+	<router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 #nav {
-  padding: 30px;
+	@apply h-16 px-16 items-center text-sm flex justify-center bg-earie text-white font-thin shadow-lg mb-4;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+	a {
+		&.router-link-exact-active {
+			@apply text-dodger underline font-normal;
+		}
+	}
 }
+
 </style>
+
+style
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 // tailwind.config.js
 module.exports = {
-    purge: [],
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {}
-    },
-    variants: {},
-    plugins: [],
+	purge: [],
+	darkMode: 'class', // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				'earie': '#1e1e1e',
+				'dodger': '#0098ff',
+				'indian': '#e09f3e',
+				'crayola': '#ed254e'
+			}
+		}
+	},
+	variants: {},
+	plugins: [],
   }
