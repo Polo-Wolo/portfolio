a922f71b9a6f8eb2854cff3b58f9900c582f42aa
Polo <polo.walczak@gmail.com>
2021-04-25 15:45:30 +0200

    ✅ NavBar Component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
-	<div id="nav">
-		<router-link to="/"><img class="h-8 w-10 ml-16 transform hover:scale-105 hover:from-transparent" src="../src/assets/PW.svg"></router-link>
-		<div class="flex-grow"></div>
-		<div class="flex-inline space-x-16 items-center">
-			<router-link to="/">Home</router-link>
-			<router-link to="/education">Education</router-link>
-			<router-link to="/projects">Projects</router-link>
-			<router-link to="/hobbies">Hobbies</router-link>
-		</div>
-		<div class="btn btn-dodger ml-64">Contact</div>
+	<div class="w-full h-screen bg-gradient-to-bl from-dodger-bg to-earie">
+		<NavBar></NavBar>
+		<router-view/>
 	</div>
-	<router-view/>
 </template>
 
-<style lang="scss">
-#nav {
-	@apply h-16 px-16 items-center text-sm flex justify-center bg-earie text-white font-thin shadow-lg mb-4;
+<script lang="ts">
+import { defineComponent } from 'vue'
+import NavBar from '@/components/NavBar.vue'
 
-	a {
-		&.router-link-exact-active {
-			@apply text-dodger underline font-normal;
-		}
+export default defineComponent({
+	components :{
+		NavBar
 	}
-}
+})
+</script>
+
+
+<style lang="scss">
 
 </style>
-
-style
