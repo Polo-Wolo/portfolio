825881e82c59dad798001253b86c599d6d3b0494
Polo <polo.walczak@gmail.com>
2021-04-26 17:50:11 +0200

    🚧 Education Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,14 @@ module.exports = {
 				dodger: {
 					light: '#33ADFF',
 					DEFAULT: '#0098ff',
+					dark: '#33ADFF',
 					bg: '#003D66',
 				},
 				'indian': '#e09f3e',
 				'crayola': '#ed254e'
+			},
+			borderWidth: {
+				'3': '3px',
 			},
 			backgroundImage: theme => ({
 				'main': "url('/background.png')"
