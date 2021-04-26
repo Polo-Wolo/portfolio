4eab196fee7545c42e9fde5ea05121e33b2781c6
Polo <polo.walczak@gmail.com>
2021-04-26 16:06:47 +0200

    💄 Update NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="nav">
-			<router-link to="/"><img class="h-8 w-10 ml-16 transform hover:scale-105 hover:from-transparent" src="../assets/PW.svg"></router-link>
+			<router-link to="/"><img class="h-8 w-10 ml-10 lg:ml-16 xl:ml-24 transform hover:scale-105 hover:from-transparent" src="../assets/PW.svg"></router-link>
 			<div class="flex-grow"></div>
-			<div class="flex-inline space-x-16 items-center">
+			<div class="flex-inline space-x-10 lg:space-x-16 items-center">
 				<router-link to="/">Home</router-link>
 				<router-link to="/education">Education</router-link>
 				<router-link to="/projects">Projects</router-link>
 				<router-link to="/hobbies">Hobbies</router-link>
 			</div>
-			<div class="btn btn-dodger ml-64">Contact</div>
+			<div class="btn btn-dodger ml-16 lg:ml-32 xl:ml-64">Contact</div>
 		</div>
 </template>
 
@@ -23,11 +23,11 @@ export default defineComponent({
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 #nav {
-	@apply h-16 px-16 items-center text-sm flex justify-center bg-earie text-white font-thin shadow-lg mb-4;
+	@apply hidden z-50 lg:flex w-full top-0 fixed h-16 px-16 items-center text-sm leading-tight justify-center bg-earie text-white font-light shadow-lg mb-4;
 
 	a {
-		&.router-link-exact-active {
-			@apply text-dodger underline font-normal;
+		&.router-link-active {
+			@apply text-dodger underline;
 		}
 	}
 }
