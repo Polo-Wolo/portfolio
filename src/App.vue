40d6b19636ef68a45c6290b538548dcf97a0aad3
Polo <polo.walczak@gmail.com>
2021-05-05 23:21:14 +0200

    🚀 Adding data.json

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
 			<NavBar></NavBar>
 		</div>
 		<div class="py-8">
-		<router-view/>
+			<router-view/>
 		</div>
+		<Footer></Footer>
 	</div>
 </template>
 
@@ -14,11 +15,19 @@
 import { defineComponent } from 'vue'
 import NavBar from '@/components/NavBar.vue'
 import MobileNavBar from '@/components/MobileNavBar.vue'
+import Footer from '@/components/Footer.vue'
+import { mapGetters } from 'vuex'
 
 export default defineComponent({
 	components :{
 		MobileNavBar,
-		NavBar
+		NavBar,
+		Footer
+	},
+	computed: {
+		...mapGetters({
+			projects: "projects"
+		})
 	}
 })
 </script>
---
+++ src/models.ts
@@ -0,0 +1,7 @@
+export interface Project {
+    title: string;
+    date: string;
+    time_spent: string;
+    content: string;
+    pictures_url: Array<string>;
+}
