7d3187231511291d6928af1457a4aac293f0f58d
Polo <polo.walczak@gmail.com>
2021-05-05 19:23:57 +0200

    💄 Add prev/next button hover effect

--- src/components/TheCarousel.vue
+++ src/components/TheCarousel.vue
@@ -1,4 +1,5 @@
 <template>
+
 	<Carousel class="flex-grow group h-full" :settings="settings">
 		<Slide v-for="slide in 5" :key="slide">
 			<div class="carousel__item">{{ slide }}</div>
@@ -8,12 +9,14 @@
 			<Navigation />
 		</template>
 	</Carousel>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import 'vue3-carousel/dist/carousel.css';
 import { Carousel, Slide, Navigation } from 'vue3-carousel';
+// import { PicturesUrl } from '@/models'
 
 export default defineComponent({
 	name: 'TheCarousel',
@@ -22,6 +25,13 @@ export default defineComponent({
 		Slide,
 		Navigation,
 	},
+	// props: {
+	// 	urls: {
+	// 		type: Object as PropType<PicturesUrl> | string,
+
+	// 	}
+	// },
+	// why incorrect ?
 	data: () => ({
 		// carousel settings
 		settings: {
@@ -35,6 +45,6 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .carousel__item {
-	@apply bg-earie flex text-white w-full justify-center items-center rounded-md min-h-300;
+	@apply bg-earie flex bg-cover bg-center text-white w-full justify-center items-center rounded-md min-h-300;
 }
 </style>
